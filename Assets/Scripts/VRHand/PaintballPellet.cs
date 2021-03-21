31c280677ad13cd1e976a43b4a232f90d9d589b9
George Wu <georgewudesign@gmail.com>
2021-03-21 16:02:48 -0400

    Paintball pellets shoot from gun and paint objects tagged "paintable"

---
+++ Assets/Scripts/VRHand/PaintballPellet.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaintballPellet : MonoBehaviour
+{
+    public Material paint;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "Paintable") 
+        {
+            collision.collider.GetComponent<Renderer>().material = paint;
+            Destroy(this.gameObject);
+        }
+    }
+}
--- Assets/Scripts/VRHand/ShootingVR.cs
+++ Assets/Scripts/VRHand/ShootingVR.cs
@@ -4,8 +4,15 @@ using UnityEngine;
 
 public class ShootingVR : GrabbableObjectVR
 {
+    public GameObject pelletPrefab;
+    public float shootingForce;
+    public Transform spawnPoint;
+
+
     public override void OnInteraction()
     {
-     
+        GameObject spawnedPellet = Instantiate(pelletPrefab, spawnPoint.position, spawnPoint.rotation);
+        spawnedPellet.GetComponent<Rigidbody>().AddForce(spawnedPellet.transform.forward * shootingForce);
+        Destroy(spawnedPellet, 2);
     }
 }
