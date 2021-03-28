c4e447a1f9c499ceccae54a793e94cd0f2bff207
George Wu <georgewudesign@gmail.com>
2021-03-28 11:05:17 -0400

    Basic snap turning via VR joystick implemented

---
+++ Assets/Scripts/VRHand/VRSnapTurn.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRSnapTurn : MonoBehaviour
+{
+    public Transform xrRig;
+    private VRInput controller;
+
+    public int angle = 20;
+    void Awake()
+    {
+        controller = GetComponent<VRInput>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(controller.thumbstick.x > 0.9f)
+        {
+            xrRig.transform.Rotate(0f, angle, 0f, Space.World);
+        }
+        else if (controller.thumbstick.x < -0.9f)
+        {
+            xrRig.transform.Rotate(0f, angle, 0f, Space.World);
+        }
+    }
+}
