2d73c948d48d0bef3b5281c471dd85dec8cacc6d
George Wu <georgewudesign@gmail.com>
2021-03-28 12:03:29 -0400

    Basic continuous movement script created. (direction of movement can be assigned to head or controller)

--- Assets/Scripts/VRHand/VRSnapTurn.cs
+++ Assets/Scripts/VRHand/VRSnapTurn.cs
@@ -13,11 +13,6 @@ public class VRSnapTurn : MonoBehaviour
         controller = GetComponent<VRInput>();
     }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
