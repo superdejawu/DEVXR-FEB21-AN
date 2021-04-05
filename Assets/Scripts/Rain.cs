aa75ca8b267b9f0ea0b257b6b65b0a633780a74f
George Wu <georgewudesign@gmail.com>
2021-04-05 11:40:19 -0400

    Animated button triggers rain

---
+++ Assets/Scripts/Rain.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rain : MonoBehaviour
+{
+
+    public GameObject shower;
+
+    private AnimatedButton button;
+    private bool follow = false;
+
+    [Tooltip("The transform to follow for rain")]
+    public Transform followTransform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        button = FindObjectOfType<AnimatedButton>();
+        button.OnButtonPressed += DoRain;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(follow == true)
+        {
+            transform.position = followTransform.position;
+        }
+    }
+
+    public void DoRain()
+    {
+        follow = !follow;
+        shower.SetActive(follow);
+    }
+
+}
--- Assets/Scripts/TouchButton.cs
+++ Assets/Scripts/TouchButton.cs
@@ -1,18 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class TouchButton : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Vector3 upPosition;
+    public Transform downTransform;
+    public Transform button;
+    /// <summary>
+    /// The thing to teleport
+    /// </summary>
+    /// 
+    [Tooltip("Thing to teleport")]
+    public Transform teleportTransform;
+    /// <summary>
+    /// The location to teleport to
+    /// </summary>
+    [Tooltip("Where to teleport")]
+    public Transform teleportPosition;
     void Start()
     {
-        
+        upPosition = transform.position;
     }
-
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.tag == "Player")
+        {
+            button.position = downTransform.position;
+            teleportTransform.position = teleportPosition.position;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            button.position = upPosition;
+        }
     }
 }
